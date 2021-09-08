4575c968ed9f7ac23c13707dc3be48eb4fb7eb94
Gopu03GPS <177gopu0312@gmail.com>
2021-09-08 16:54:58 +0530

    initial: starter code for starting frontend

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,1 +1,14 @@
 // Home page, basically we use the reusable components here in pages.
+import React, {Component} from 'react';
+
+class Home extends Component {
+    render() {
+        return (
+            <div className="welcome">
+                Welcome to GDSC AIT, Pune.
+            </div>
+        );
+    }
+}
+
+export default Home;
