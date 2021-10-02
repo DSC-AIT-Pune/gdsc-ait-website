9eb4434a9a2935adc356150d26eaf0b8f480db00
robin00007 <76676218+robin00007@users.noreply.github.com>
2021-10-02 18:11:37 +0530

    footer added

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -8,6 +8,7 @@ import Github from "../images/Github.png";
 import facebook from "../images/facebook.png";
 import linkdIn from "../images/linkedIn.png";
 import Discription from "./home_components/discription";
+import Social from "./home_components/social";
 class Home extends Component {
   state = {
     dis: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in",
@@ -23,42 +24,41 @@ class Home extends Component {
   render() {
     return (
       <div className="frontPage">
-         <div className="welcome">
-        {/* <img src={bg_svg} alt="" className='bg_svg' /> */}
-        {/* <img src={movingCrossBgImage} alt="" className="rotate" />
+        <div className="welcome">
+          {/* <img src={bg_svg} alt="" className='bg_svg' /> */}
+          {/* <img src={movingCrossBgImage} alt="" className="rotate" />
         <img src={movingCrossBgImage} alt="" className="rotate2" /> */}
-        <div className="content">
-          <h1 className="Title_heading">Lorem ipsum </h1>
-          <br />
-          <p>{this.state.dis}</p>
-          <button className="Member_button">Join as member</button>
-          <button href="dfsdf" className="Discord_button">
-            Join Discord
-          </button>
-          <div className="socialMediaLink">
-            {this.state.socialMedia.map((social_Media) => (
-              <a href={social_Media.url}>
-                <img
-                  src={social_Media.src}
-                  alt={social_Media.id}
-                  className="social_Media_logo"
-                />
-              </a>
-            ))}
+          <div className="content">
+            <h1 className="Title_heading">Lorem ipsum </h1>
+            <br />
+            <p>{this.state.dis}</p>
+            <button className="Member_button">Join as member</button>
+            <button href="dfsdf" className="Discord_button">
+              Join Discord
+            </button>
+            <div className="socialMediaLink">
+              {this.state.socialMedia.map((social_Media) => (
+                <a href={social_Media.url}>
+                  <img
+                    src={social_Media.src}
+                    alt={social_Media.id}
+                    className="social_Media_logo"
+                  />
+                </a>
+              ))}
+            </div>
+          </div>
+          <div className="HomePage_svg">
+            <img src={image} alt="fdsfsdf" className="svg" />
           </div>
         </div>
-        <div className="HomePage_svg">
-          <img src={image} alt="fdsfsdf" className="svg" />
+        <div className="discription_container">
+          <Discription />
+        </div>
+        <div className="SocialMediaContainer">
+          <Social />
         </div>
       </div>
-      <div className="discription_container">
-      <Discription/>
-      </div>
-
-
-
-      </div>
-      
     );
   }
 }
---
+++ client/src/pages/home_components/social.js
@@ -0,0 +1,66 @@
+import React, { useState } from "react";
+import map from "../../images/Map.svg";
+import insta from "./disImg/insta.png";
+import Github from "./disImg/Github.png";
+import facebook from "./disImg/facebook.png";
+import linkdIn from "./disImg/linkedIn.png";
+
+const Social = () => {
+  const [state, setState] = useState([
+    { url: "fdsf", id: "1", src: insta },
+    { url: "fdf", id: "3", src: Github },
+    { url: "fdfd", id: "4", src: facebook },
+    { url: "ddfs", id: "5", src: linkdIn },
+  ]);
+  return (
+    <div className="SocialMedia">
+      <div className="map">
+        <div>
+          <h3>Where you can find us ?</h3>
+          <h1>Google Developer Student Club</h1>
+        </div>
+
+        <img src={map} alt="" />
+      </div>
+
+      <div className="socialLinks">
+        
+        <div className="socialDetails">
+          <h1>GDSC AIT</h1>
+          <div className="socialAddesss">
+            <div className="s1">
+              <p>contact</p>
+              <p>+91-9483958384</p>
+              <p>dsc.aitpune@gmail.com</p>
+            </div>
+            <div className="s1">
+              <p>Address</p>
+              <p>Alandi Road </p>
+              <p>Dighi Hills</p>
+              <p>Pune-411015 </p>
+            </div>
+            <div className="s1">
+              <p>Dev Programs</p>
+              <p>Google Solutions</p>
+              <p>Study JAMs</p>
+              <p>Flutter</p>
+            </div>
+          </div>
+          <div className="socialMediaLink s4">
+            {state.map((social_Media) => (
+              <a href={social_Media.url}>
+                <img
+                  src={social_Media.src}
+                  alt={social_Media.id}
+                  className="social_Media_logo"
+                />
+              </a>
+            ))}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Social;
