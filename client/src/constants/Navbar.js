e1ab109513863ce7303fd732dbe861e5052ee928
impetus007 <vishal2219singh@gmail.com>
2021-10-03 18:51:44 +0530

    add

--- client/src/constants/Navbar.js
+++ client/src/constants/Navbar.js
@@ -4,3 +4,10 @@
  *      these can be defined here in the form of array of objects. Each object
  *      containing path, name, id - related to that particular option.
  */
+
+/**
+ * File which will contain constant to be used in Navbar.
+ * Example: The options that we display in navbar (about, projects, home etc),
+ *      these can be defined here in the form of array of objects. Each object
+ *      containing path, name, id - related to that particular option.
+ */
