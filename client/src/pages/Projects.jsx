34bf3ed417dc4beb16f1322ee1f2709bdc9b20e1
Gopu03GPS <177gopu0312@gmail.com>
2021-10-03 14:53:37 +0530

    projects.jsx added

---
+++ client/src/pages/Projects.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Projects = () => {
+    return (
+        <div className="project-container">
+            <div style={{fontSize: "40px", fontWeight: "bold" , margin: "250px"}}>
+                Coming Soon...
+            </div>
+        </div>
+    );
+};
+
+export default Projects;
