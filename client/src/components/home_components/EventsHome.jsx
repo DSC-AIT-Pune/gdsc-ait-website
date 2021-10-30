d3b22e502863ee850829e22c52b826c4f2911b54
Gopu03GPS <177gopu0312@gmail.com>
2021-10-30 14:26:02 +0530

    feat: events added at home

--- client/src/components/home_components/EventsHome.jsx
+++ client/src/components/home_components/EventsHome.jsx
@@ -1,8 +1,16 @@
 import React from "react";
+import "../../styles/EventsHome.css";
 
-const EventsHome = () => {
+const EventsHome = ({img, title, description, ... rest}) => {
     return (
-        <p>Events</p>
+        <div className="event-home-card-container">
+            <div className="event-home-card-body">
+                <img className="event_img" src={img} />
+                <h2 className="event_title">{title}</h2>
+                <p className="event_description">{description}</p>
+            </div>
+            <button className="event-home-card-btn">Know More...</button>
+        </div>
     );
 }
 
