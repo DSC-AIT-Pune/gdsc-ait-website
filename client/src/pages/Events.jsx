303f241ddc715e43b0cd88e22e164be9d611adfe
Ajay <ajaysingh07032003@gmal.com>
2022-10-10 23:00:39 +0530

    Merge pull request #43 from ajaysinghpanwar2002/ajay
    getting data from json file, passed data as props

--- client/src/pages/Events.jsx
+++ client/src/pages/Events.jsx
@@ -2,165 +2,62 @@ import React from "react";
 import Ofest from "./ofest";
 import EventGrid from "../components/events_components/EventsGrid";
 import ExpandedLayout from "../components/events_components/ExpandLayout";
-import {Grid, Box} from '@mui/material'
+import { Grid, Box } from '@mui/material'
 import EventTimelineImage from '../styles/images/event_timeline.jpg'
 import styles from '../styles/pages/events/events.module.css'
 
+import MajorEvent from "../database/MajorEvent";
+import MinorEvents from "../database/MinorEvents";
+
 const Events = () => {
 
-  
-  let majorEvents = [
-    {
-        name: "30 Days Of Cloud",
-        img: "https://firebasestorage.googleapis.com/v0/b/gdsc-website-media.appspot.com/o/gdsc-website%2Fevent-banner%2F30DaysOfCloud.png?alt=media&token=876d3456-5598-4787-87ba-e9259462cd8f",
-        logo: "https://firebasestorage.googleapis.com/v0/b/gdsc-website-media.appspot.com/o/gdsc-website%2F30DaysOfCloud%2F30DaysOfCloud-logo.png?alt=media&token=9b241f2d-e669-410c-9087-915e76b8644e",
-        url: "https://gdsc.community.dev/events/details/developer-student-clubs-army-institute-of-technology-pune-presents-30daysofcloud-orientation/",
-        desc: "30 Days of Google Cloud program will provide you an opportunity to kickstart your career in the cloud"
-    },
-    {
-        name: "Android Study Jams",
-        img: "https://firebasestorage.googleapis.com/v0/b/gdsc-website-media.appspot.com/o/gdsc-website%2Fandroid-study-jams%2Fandroid-study-jams.jpeg?alt=media&token=7a00e80a-4dfb-4990-8ed9-fdd2f92ebfed",
-        logo: "https://firebasestorage.googleapis.com/v0/b/gdsc-website-media.appspot.com/o/gdsc-website%2Fhackotoberfest%2FH-icon-dark.png?alt=media&token=777c1020-3747-44d5-8f69-acf6ce18e887",
-        url: "https://gdsc.community.dev/events/details/developer-student-clubs-army-institute-of-technology-pune-presents-30daysofcloud-orientation/",
-        desc: "30 Days of Google Cloud program will provide you an opportunity to kickstart your career in the cloud"
-    },
-    {
-        name: "Flutter Engage",
-        img: "https://firebasestorage.googleapis.com/v0/b/gdsc-website-media.appspot.com/o/gdsc-website%2Fflutter-engage%2Fflutter-engage-sharing.jpg?alt=media&token=40adfed7-df42-443f-a6ff-b822b4c1cb75",
-        logo: "https://firebasestorage.googleapis.com/v0/b/gdsc-website-media.appspot.com/o/gdsc-website%2Fhackotoberfest%2FH-icon-dark.png?alt=media&token=777c1020-3747-44d5-8f69-acf6ce18e887",
-        url: "https://gdsc.community.dev/events/details/developer-student-clubs-army-institute-of-technology-pune-presents-30daysofcloud-orientation/",
-        desc: "30 Days of Google Cloud program will provide you an opportunity to kickstart your career in the cloud"
-    },
-    {
-        name: "Google Solution Challenge",
-        img: "https://firebasestorage.googleapis.com/v0/b/gdsc-website-media.appspot.com/o/gdsc-website%2FGoogle-Solution-Challenge%2Fgdsc_2021_header.png?alt=media&token=90b33635-be76-4e47-80f5-cd1518228486",
-        logo: "https://firebasestorage.googleapis.com/v0/b/gdsc-website-media.appspot.com/o/gdsc-website%2Fhackotoberfest%2FH-icon-dark.png?alt=media&token=777c1020-3747-44d5-8f69-acf6ce18e887",
-        url: "https://gdsc.community.dev/events/details/developer-student-clubs-army-institute-of-technology-pune-presents-30daysofcloud-orientation/",
-        desc: "30 Days of Google Cloud program will provide you an opportunity to kickstart your career in the cloud"
-    },
-    {
-        name: "Explore ML",
-        img: "https://firebasestorage.googleapis.com/v0/b/gdsc-website-media.appspot.com/o/gdsc-website%2Fexplore-ml%2Fexplore-ml.jpg?alt=media&token=f3694cee-de93-4495-aaad-197a12fa932e",
-        logo: "https://firebasestorage.googleapis.com/v0/b/gdsc-website-media.appspot.com/o/gdsc-website%2Fhackotoberfest%2FH-icon-dark.png?alt=media&token=777c1020-3747-44d5-8f69-acf6ce18e887",
-        url: "https://gdsc.community.dev/events/details/developer-student-clubs-army-institute-of-technology-pune-presents-30daysofcloud-orientation/",
-        desc: "30 Days of Google Cloud program will provide you an opportunity to kickstart your career in the cloud"
-    }
-  ]
+    const MajorEvents = MajorEvent.map((event, ind) => {
+        return (
+            <ExpandedLayout
+                id={ind}
+                img={event.img}
+                text={event.desc}
+                heading={event.name}
+                url={event.url} 
+                />
+        )
+    });
 
-  let minorEvents = [
-    {
-        name: "#30DaysOfCloud: Orientation",
-        banner: "https://firebasestorage.googleapis.com/v0/b/gdsc-website-media.appspot.com/o/gdsc-website%2Fevent-banner%2F30DaysOfCloud.png?alt=media&token=876d3456-5598-4787-87ba-e9259462cd8f",
-        logo: "https://firebasestorage.googleapis.com/v0/b/gdsc-website-media.appspot.com/o/gdsc-website%2F30DaysOfCloud%2F30DaysOfCloud-logo.png?alt=media&token=9b241f2d-e669-410c-9087-915e76b8644e",
-        url: "https://gdsc.community.dev/events/details/developer-student-clubs-army-institute-of-technology-pune-presents-30daysofcloud-orientation/",
-        desc: "30 Days of Google Cloud program will provide you an opportunity to kickstart your career in the cloud."
-    },
-    {
-        name: "Hacktoberfest",
-        banner: "https://firebasestorage.googleapis.com/v0/b/gdsc-website-media.appspot.com/o/gdsc-website%2Fhackotoberfest%2Fbanner08-light.png?alt=media&token=b70219af-f468-4139-a707-f9dd9627f2ba",
-        logo: "https://firebasestorage.googleapis.com/v0/b/gdsc-website-media.appspot.com/o/gdsc-website%2Fhackotoberfest%2FH-icon-dark.png?alt=media&token=777c1020-3747-44d5-8f69-acf6ce18e887",
-        url: "https://gdsc.community.dev/events/details/developer-student-clubs-army-institute-of-technology-pune-presents-30daysofcloud-orientation/",
-        desc: "Start your open-source journey while earning a branded Hacktoberfest T-shirt."
-    },
-    {
-        name: "DevLok",
-        banner: "https://firebasestorage.googleapis.com/v0/b/gdsc-website-media.appspot.com/o/gdsc-website%2Fdevlok%2Fdevlok-poster.jpg?alt=media&token=d1b5c64a-62a9-4d8f-a38f-96154d5854f7",
-        logo: "https://firebasestorage.googleapis.com/v0/b/gdsc-website-media.appspot.com/o/gdsc-website%2Fgoogle-core-web-vitals-5eb1b37825ad1-760x400.png?alt=media&token=4beb587c-c9f7-47ef-b3cf-00ea6e1c65a4",
-        url: "https://gdsc.community.dev/events/details/developer-student-clubs-army-institute-of-technology-pune-presents-30daysofcloud-orientation/",
-        desc: "GDSC AIT & AIT OSS Club presents DevLok, this year's first event. Exclusive for AIT students, this event is a platform for students to learn new skillsets and tech-stacks under the guidance of our experienced mentors."
-    },
-    {
-        name: "New To Programming Track: Session 2",
-        banner: "https://firebasestorage.googleapis.com/v0/b/gdsc-website-media.appspot.com/o/gdsc-website%2Fandroid-study-jams%2Fandroid-study-jams.jpeg?alt=media&token=7a00e80a-4dfb-4990-8ed9-fdd2f92ebfed",
-        logo: "https://miro.medium.com/max/1400/1*yIrYl18oa_jtivCrJEKvYw.png",
-        url: "https://gdsc.community.dev/events/details/developer-student-clubs-army-institute-of-technology-presents-new-to-programming-track-session-2/",
-        desc: "Learn the basics of building Android apps with the Kotlin programming language and develop a collection of simple apps to start your journey as an Android developer."
-    },
-    {
-        name: "Android Study JAM Session for Prior Programmers",
-        banner: "https://firebasestorage.googleapis.com/v0/b/gdsc-website-media.appspot.com/o/gdsc-website%2Fandroid-study-jams%2Fads21-jetpack-compose-material-you_960.png?alt=media&token=0535d5d4-296e-4cc7-8b92-da885e6d3559",
-        logo: "https://miro.medium.com/max/1400/1*yIrYl18oa_jtivCrJEKvYw.png",
-        url: "https://gdsc.community.dev/events/details/developer-student-clubs-army-institute-of-technology-presents-android-study-jam-session-for-prior-programmers/",
-        desc: "First learn the essentials of the Kotlin programming language. Then learn the fundamentals of Android development and best practices by building a variety of Android apps in Kotlin."
-    },
-    {
-        name: "Android Study JAM Session for Beginners - Session 1",
-        banner: "https://firebasestorage.googleapis.com/v0/b/gdsc-website-media.appspot.com/o/gdsc-website%2Fandroid-study-jams%2Fads21-modern-android-development_1920.png?alt=media&token=e450f7c5-9b27-4516-842e-e5131322428b",
-        logo: "https://miro.medium.com/max/1400/1*yIrYl18oa_jtivCrJEKvYw.png",
-        url: "https://gdsc.community.dev/events/details/developer-student-clubs-army-institute-of-technology-presents-android-study-jam-session-for-beginners-session-1/",
-        desc: "Learn the basics of building Android apps with the Kotlin programming language and develop a collection of simple apps to start your journey as an Android developer."
-    },
-    {
-        name: "Introduction to DevOps and Cloud Technologies",
-        banner: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQSZ-9i2ON_XW4laWKyT9SNY7VN2O2K1aOn1e1pO7C1--DZjJuA9cc50shNQ7eSivBaZlg&usqp=CAU",
-        logo: "https://cdn.iconscout.com/icon/free/png-256/azure-devops-3628645-3029870.png",
-        url: "https://gdsc.community.dev/events/details/developer-student-clubs-army-institute-of-technology-presents-introduction-to-devops-and-cloud-technologies/",
-        desc: "The session will give you a good insight into DevOps, what it is and you will be given knowledge about the various paths which you can follow in this field according to your interests."
-    },
-    {
-        name: "DSC-AIT ML,DL and Tensorflow Workshop",
-        banner: "https://firebasestorage.googleapis.com/v0/b/gdsc-website-media.appspot.com/o/gdsc-website%2Fexplore-ml%2Ftf_dribbb.webp?alt=media&token=73fd8574-5d92-4df9-88db-de5615020227",
-        logo: "https://upload.wikimedia.org/wikipedia/commons/thumb/2/2d/Tensorflow_logo.svg/1200px-Tensorflow_logo.svg.png",
-        url: "https://gdsc.community.dev/events/details/developer-student-clubs-army-institute-of-technology-pune-presents-30daysofcloud-orientation/",
-        desc: "DSC_AIT Pune, in association with Deep Info Lab, brings you a series of seminars, to give you a head start in your ML and Deep Learning journey."
-    },
-    {
-        name: "Introduction to GSoC & Past Experience Talk",
-        banner: "https://athityakumar.github.io/assets/images/blog/GSoC_2017_-_Acceptance_of_Student_Proposal/banner.png",
-        logo: "https://upload.wikimedia.org/wikipedia/commons/thumb/8/85/GSoC-icon.svg/1024px-GSoC-icon.svg.png",
-        url: "https://gdsc.community.dev/events/details/developer-student-clubs-army-institute-of-technology-presents-introduction-to-gsoc-past-experience-talk/",
-        desc: "Google Summer of Code is a global program focused on bringing more student developers into open source software development."
-    }
-  ]
-
-
-  const MajorEvents = majorEvents.map((event,ind) => {
-    return(
-    <ExpandedLayout 
-      id={ind} 
-      img={event.img} 
-      text={event.desc} 
-      heading={event.name} 
-      url={event.url} />
-    )
-  });
-
-  return (
-      <div className={styles.eventSection}>
-        <section className={styles.bannerSection}>
-            <Grid container lg={12} md={12} xs={12} className={styles.bannerSectionContainer}>
-                <Grid item lg={7} md={12} xs={12}>
-                    <img className={styles.bannerImage} src={EventTimelineImage} />
+    return (
+        <div className={styles.eventSection}>
+            <section className={styles.bannerSection}>
+                <Grid container lg={12} md={12} xs={12} className={styles.bannerSectionContainer}>
+                    <Grid item lg={7} md={12} xs={12}>
+                        <img className={styles.bannerImage} src={EventTimelineImage} />
+                    </Grid>
+                    <Grid item lg={5} md={12} xs={12} className={styles.bannerContent}>
+                        <div className={styles.bannerHeading}><h1>{"Participate and Engage"}</h1></div>
+                        <div className={styles.bannerSubText}><span>{"We host events and hackathons all year long, where you may learn new skills by putting your knowledge and talents to use in order to win prizes."}</span></div>
+                    </Grid>
                 </Grid>
-                <Grid item lg={5} md={12} xs={12} className={styles.bannerContent}>
-                    <div className={styles.bannerHeading}><h1>{"Participate and Engage"}</h1></div>                   
-                    <div className={styles.bannerSubText}><span>{"We host events and hackathons all year long, where you may learn new skills by putting your knowledge and talents to use in order to win prizes."}</span></div>
-                </Grid>
-            </Grid>
-        </section>
-        <div className={styles.sectionHeading} >
-            <span >{"Major Events"}</span>
-        </div>
+            </section>
+            <div className={styles.sectionHeading} >
+                <span >{"Major Events"}</span>
+            </div>
 
-        <section className={styles.sectionStyle}>
-          {MajorEvents}
-        </section>
-        
-        <div className={styles.sectionHeading} >
-            <span >{"Sessions"}</span>
-        </div>
-        
-       {/* <div className={styles.eventTimeline} >
+            <section className={styles.sectionStyle}>
+                {MajorEvents}
+            </section>
+
+            <div className={styles.sectionHeading} >
+                <span >{"Sessions"}</span>
+            </div>
+
+            {/* <div className={styles.eventTimeline} >
               <div><span className={styles.eventTimelineItems}>{"Previous"}</span></div>
               <div><span className={styles.eventTimelineItems}>{"Current"}</span></div>
               <div><span className={styles.eventTimelineItems}>{"Upcomming"}</span></div>
-        </div> */} 
-        <section style={{marginTop:"48px", marginBottom:"32px"}}><EventGrid events={minorEvents}/></section>
-        
-      </div>
-   
+        </div> */}
+            <section style={{ marginTop: "48px", marginBottom: "32px" }}><EventGrid events={MinorEvents} /></section>
 
-  );
+        </div>
+
+    );
 };
 
 export default Events;
--- client/src/pages/Team.jsx
+++ client/src/pages/Team.jsx
@@ -6,133 +6,11 @@ import { Grid } from "@mui/material";
 import styles from "../styles/pages/team/team.module.css"
 import Banner from "../common/Banner";
 
+import Teammembers from "../database/Teammembers";
+
 const MEMBER_URI = "http://localhost:5000/api/v1/member/all"
 
 class Team extends React.Component {
-  state = {
-    members: [
-      {
-        name: "Shekhar Kumar",
-        position: "GDSC LEAD",
-        image_url: "https://avatars.githubusercontent.com/u/51144829?v=4",
-        social_profiles:{
-            gdsc: "https://gdsc.community.dev/u/shekhar/",
-            linkedIn: "https://www.linkedin.com/in/the-anton//",
-            gitHub: "https://github.com/The-Anton",
-            facebook: "https://www.facebook.com/profile.php?id=100005728093339",
-            instagram: "https://www.instagram.com/the_anton.01/",
-        }
-      },
-      {
-        name: "Yash Chaudhary",
-        position: "Machine Learning",
-        image_url: "https://user-images.githubusercontent.com/51144829/139212689-e3fbdd9b-f964-4460-ae38-f48bf9102fb9.jpg",
-        social_profiles:{
-            gdsc: "https://gdsc.community.dev/u/mwt5zp/",
-            linkedIn: "https://www.linkedin.com/in/yash-chaudhary-000a4b16a",
-            gitHub: "https://github.com/",
-            facebook: "https://www.facebook.com/jaatbuoy",
-            instagram: "https://www.instagram.com/_chacha._.chaudhary_/",
-        }
-      },
-      {
-        name: "Abhishek Kumar Meel",
-        position: "Managment Head",
-        image_url: "https://avatars.githubusercontent.com/u/72295943?v=4",
-        social_profiles:{
-            gdsc: "https://gdsc.community.dev/u/m2undb/",
-            linkedIn: "https://www.linkedin.com/in/abhishek-kumar-meel",
-            gitHub: "https://github.com/",
-            facebook: "https://www.facebook.com/abhishekmeel29",
-            instagram: "https://www.instagram.com/abhishek.meel29",
-        }
-      },
-      {
-        name: "Navneet Das",
-        position: "Android & Cloud Head",
-        image_url: "https://avatars.githubusercontent.com/u/62807226?v=4",
-        social_profiles:{
-            gdsc: "https://gdsc.community.dev/u/mw5655/#/about",
-            linkedIn: "https://www.linkedin.com/in/navneetdas19/",
-            gitHub: "https://github.com/MIDAS1901",
-            facebook: "https://www.facebook.com/profile.php?id=100009876039560",
-            instagram: "www.instagram.com/navneet.1901",
-        }
-      },
-      {
-        name: "Chandresh Singh",
-        position: "Android",
-        image_url: "https://avatars.githubusercontent.com/u/54761454?v=4",
-        social_profiles:{
-            gdsc: "https://gdsc.community.dev/u/mytutb/",
-            linkedIn: "https://www.linkedin.com/in/chandresh-singh-dev/",
-            gitHub: "https://github.com/The-Pascal",
-            facebook: "https://www.facebook.com/Chandresh.Singh.dev/",
-            instagram: "https://www.instagram.com/chandreshmg/",
-        }
-      },
-      {
-        name: "Govind Prasad Sharma",
-        position: "Web Technologies",
-        image_url: "https://avatars.githubusercontent.com/u/59963674?v=4",
-        social_profiles:{
-            gdsc: "https://gdsc.community.dev/u/mytutb/",
-            linkedIn: "https://www.linkedin.com/in/govind-prasad-sharma-21859a1b0/",
-            gitHub: "https://github.com/Gopu03GPS",
-            facebook: "https://www.facebook.com/govind.prasadsharma.37/",
-            instagram: "https://www.instagram.com/hey_g0pu/?hl=en",
-        }
-      },
-      {
-        name: "Rishabh Rathore",
-        position: "UI/UX",
-        image_url: "https://avatars.githubusercontent.com/u/56212029?v=4",
-        social_profiles:{
-            gdsc: "https://gdsc.community.dev/u/mnc2qq/",
-            linkedIn: "https://www.linkedin.com/in/xerycks/",
-            gitHub: "https://github.com/xerycks",
-            facebook: "https://www.facebook.com/xerycks/",
-            instagram: "https://www.instagram.com/xerycks/",
-        }
-      },
-      {
-          name: "Samik Choudhury",
-          position: "Android & PR",
-          image_url: "https://avatars.githubusercontent.com/u/73891260?v=4",
-          social_profiles:{
-              gdsc: "https://gdsc.community.dev/u/mytutb/",
-              linkedIn: "https://www.linkedin.com/in/samik15/",
-              gitHub: "https://github.com/licofiS",
-              facebook: "https://www.facebook.com/samik.2025/",
-              instagram: "https://www.instagram.com/sa.m.ik/",
-          }
-      },
-      {
-        name: "Robin",
-        position: "Web Technologies",
-        image_url: "https://avatars.githubusercontent.com/u/76676218?v=4",
-        social_profiles:{
-            gdsc:"https://gdsc.community.dev/u/m4u2s2/",
-            linkedIn: "www.linkedin.com/in/robinhood00007",
-            gitHub: "https://github.com/robin00007",
-            facebook:"https://www.facebook.com/robin.choudhary.3745496/",
-            instagram: "https://www.instagram.com/robinhood062/",
-        },
-      },
-      {
-        name: "Tarun Mishra",
-        position: "Web and UI/UX",
-        image_url: "https://avatars.githubusercontent.com/u/32845547",
-        social_profiles:{
-            gdsc: "https://gdsc.community.dev/u/m5qsb2/",
-            linkedIn: "https://www.linkedin.com/in/codestein-tarun",
-            gitHub: "https://github.com/Illusion0-0",
-            facebook: "https://www.facebook.com/Optimus.404",
-            instagram: "https://www.instagram.com/codestein_",
-        }
-      },
-    ]
-  }
 
   componentDidMount() {
     // axios.get(MEMBER_URI).then((res)=>{
@@ -142,25 +20,25 @@ class Team extends React.Component {
     // })
   }
 
-  render(){
-    return(
+  render() {
+    return (
       <div className={styles.team}>
-            <Banner image={TeamImage} 
-              color="#6199F6"
-              heading={"Community that grows by embracing others"}
-              subtext={"We are team of experienced developers who are interested in giving back to the community"}/>
-            
+        <Banner image={TeamImage}
+          color="#6199F6"
+          heading={"Community that grows by embracing others"}
+          subtext={"We are team of experienced developers who are interested in giving back to the community"} />
+
         <div>
-            <div>
-              <span style={{fontSize: "8vh", fontWeight: "bold", color: "#4285F4"}}>{"Our Team"}</span>
-            </div>
-            <div className={styles.yearTimeline} >
-              <div><span className={styles.timelineItems}>{"2021"}</span></div>
-              <div><span className={styles.timelineItems}>{"Alumni"}</span></div>
-            </div>
+          <div>
+            <span style={{ fontSize: "8vh", fontWeight: "bold", color: "#4285F4" }}>{"Our Team"}</span>
           </div>
+          <div className={styles.yearTimeline} >
+            <div><span className={styles.timelineItems}>{"2021"}</span></div>
+            <div><span className={styles.timelineItems}>{"Alumni"}</span></div>
+          </div>
+        </div>
         <section className={styles.member}>
-          {this.state.members.map((member, ind) => {
+          {Teammembers.map((member, ind) => {
             return (
               <TeamGrid
                 key={ind}
@@ -174,11 +52,10 @@ class Team extends React.Component {
               />
             );
           })}
-          
         </section>
-    </div>
+      </div>
     )
-  } 
+  }
 }
 
 export default Team;
